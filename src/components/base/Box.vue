e8bb8eb38b5e2a0b0bdee86153f79b780f29941a
“Vinson” <xiesenwen@qq.com>
2023-08-13 19:49:59 +0800

    chore: 样式调整

--- src/components/base/Box.vue
+++ src/components/base/Box.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="max-w-[930px] rounded-lg bg-white p-3 shadow-[0_0_1rem_rgba(0,0,0,0.1)] dark:bg-neutral-800 dark:shadow-[0_0_1rem_#000000] md:p-5"
+    class="max-w-[930px] bg-white p-3 shadow-[0_0_1rem_rgba(0,0,0,0.1)] dark:bg-neutral-800 dark:shadow-[0_0_1rem_#000000] md:rounded-lg md:p-5"
   >
     <slot />
   </div>
