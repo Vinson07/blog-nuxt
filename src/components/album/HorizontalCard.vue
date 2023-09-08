366f7320c8fcd3c5e26d1f6cd70a77270add2470
Vinson <153934328@qq.com>
2023-09-08 09:46:23 +0800

    fix: 相册图片问题修复

--- src/components/album/HorizontalCard.vue
+++ src/components/album/HorizontalCard.vue
@@ -14,9 +14,14 @@ withDefaults(defineProps<Props>(), {
 
 <template>
   <div class="relative h-[410px] animate-[slideBottomIn_1s] overflow-hidden rounded-3xl">
-    <video autoplay muted loop class="h-full w-full object-cover">
+    <!-- <video autoplay muted loop class="h-full w-full object-cover">
       <source src="https://cdn.sakura520.co/static/video/hutao.mp4" type="video/mp4" />
-    </video>
+    </video> -->
+    <img
+      class="h-full w-full object-cover"
+      src="https://cdn.sakura520.co/static/images/tumblr_752e98a41362e1c7e51c7a50a78c179c_187cb60b_1280.gif"
+      alt=""
+    />
     <div class="absolute top-0 left-0 h-full p-3 md:p-5">
       <div class="p-10px">
         <h5 class="text-15 text-white">{{ tip }}</h5>
